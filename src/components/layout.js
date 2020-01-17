e4c06de93b7981f416073b93049d13ffbb290077
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2020-01-17 22:58:18 +0100

    First paragraphs

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,7 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            fontSize: '1.2rem',
+            fontSize: `1.2rem`,
             marginBottom: rhythm(1.5),
             marginTop: 0,
           }}
@@ -34,7 +34,7 @@ class Layout extends React.Component {
       header = (
         <h3
           style={{
-            fontSize: '1.2rem',
+            fontSize: `1.2rem`,
             fontFamily: `Montserrat, sans-serif`,
             marginTop: 0,
           }}
