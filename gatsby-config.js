79b4aece2479c2396aa89600fd9168ff8d03d82e
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2021-03-26 22:39:33 +0100

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-5XK3BEY1LC`,
       },
     },
     `gatsby-plugin-feed`,
